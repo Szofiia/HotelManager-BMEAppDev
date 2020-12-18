b0151226dccce1d7c97674d676d9c39ea1e376f1
Szofiia <zsofia.solymosi.dev@gmail.com>
2020-12-18 15:38:51 +0100

    feat: add database

---
+++ HotelManager/HotelManager/data/IDataBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HotelManager.model;
+
+namespace HotelManager.data
+{
+    public interface IDataBase
+    {
+        void AddGuest(Guest guest);
+        void AddReservation(Reservation reservation);
+        void AddRoom(Room room);
+        List<Room> GetAvailableRooms(DateTime arrival, DateTime departure);
+        List<Room> GetRooms();
+        Reservation GetReservation(long reservationId);
+        Room GetRoomByNumber(int number);
+        int GetChildCost();
+    }
+}
